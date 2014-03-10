4cf37eb81342925bb3537c1678546d50b82f54f7
Joe Cheng <joe@rstudio.org>
2014-03-10 11:08:53 -0700

    Upgrade to Leaflet 0.7.2, make demo https-friendly

--- R/leaflet.R
+++ R/leaflet.R
@@ -94,11 +94,8 @@ leafletMap <- function(
   tagList(
     singleton(
       tags$head(
-        HTML('<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.css" />
-<!--[if lte IE 8]>
-  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5/leaflet.ie.css" />
-<![endif]-->
-<script src="http://cdn.leafletjs.com/leaflet-0.5/leaflet.js"></script>'),
+        tags$link(rel="stylesheet", type="text/css", href="leaflet/leaflet.css"),
+        tags$script(src="leaflet/leaflet.js"),
         tags$script(src="leaflet/binding.js")
       )
     ),
--- inst/example/ui.R
+++ inst/example/ui.R
@@ -5,7 +5,7 @@ shinyUI(fluidPage(
   tags$head(tags$link(rel='stylesheet', type='text/css', href='styles.css')),
   leafletMap(
     "map", "100%", 400,
-    initialTileLayer = "http://{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+    initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
     initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
     options=list(
       center = c(37.45, -93.85),
