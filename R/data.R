8b744ca84d66e069162e4ec60608bd0e69abf3ee
Yihui Xie <xie@yihui.name>
2014-10-24 15:51:54 -0500

    I think this should be a portable way to store data with non-ASCII characters
    read.csv(fileEncoding = 'UTF-8') should convert characters to native encoding on Windows

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' City and town intercensal US population estimates (2000-2010)
+#'
+#' Intercensal estimates of the resident population for Incorporated Places and
+#' Minor Civil Divisions: April 1, 2000 to July 1, 2010.
+#' @docType data
+#' @format A data frame containing \code{City}, \code{State}, \code{Lat},
+#'   \code{Long}, and population estimates from 2000 to 2010 (columns
+#'   \code{Pop2000} to \code{Pop2010}).
+#' @source The US Census Bureau:
+#'   \url{http://www.census.gov/popest/data/intercensal/cities/cities2010.html}
+#' @export
+#' @examples library(leaflet)
+#' str(uspop2000)
+#' p <- uspop2000$Pop2010
+#' p <- (p - min(p))/(max(p) - min(p))
+#' plot(Lat ~ Long, data = uspop2000, cex = sqrt(p), asp = 1, col = rgb(1, 0, 0, .3))
+uspop2000 <- NULL
+if (file.exists('inst/csv/uspop2000.csv')) {
+  uspop2000 <- read.csv('inst/csv/uspop2000.csv', fileEncoding = 'UTF-8')
+}
