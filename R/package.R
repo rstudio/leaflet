a8ca24f2679dd697b8a19837c48c920876a19878
Yihui Xie <xie@yihui.name>
2014-11-26 00:58:15 -0600

    export %>% from magrittr

---
+++ R/package.R
@@ -0,0 +1,10 @@
+#' Re-exported objects from other packages
+#'
+#' This page is only for the purpose of \command{R CMD check}, because all
+#' exported objects must be documented, even if they are only objects
+#' re-exported from other packages, where they have been documented.
+#' @importFrom magrittr %>%
+#' @export %>%
+#' @aliases %>%
+#' @name leaflet-exports
+NULL
