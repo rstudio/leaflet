7e8e87b4c427e7c4c8841a283929266129b39dfb
Yihui Xie <xie@yihui.name>
2015-05-06 10:26:15 -0500

    cosmetic: re-wrap roxygen comments, add spaces, arrow to equal, etc

--- R/controls.R
+++ R/controls.R
@@ -1,26 +1,24 @@
-#' @param html the content of the control. May be provided as string or as
-#'   HTML generated with Shiny/htmltools tags
-#' @param position position of control: 'topleft', 'topright',
-#'   'bottomleft', or 'bottomright'
+#' @param html the content of the control. May be provided as string or as HTML
+#'   generated with Shiny/htmltools tags
+#' @param position position of control: 'topleft', 'topright', 'bottomleft', or
+#'   'bottomright'
 #' @param controlId the id of this control
 #' @param classes extra CSS classes to append to the control
 #'
 #' @describeIn map-layers Add arbitrary HTML controls to the map
 #' @export
-addControl = function(map,
-    html,
-    position=c('topleft', 'topright', 'bottomleft', 'bottomright'),
-    controlId=NULL,
-    classes=c('info', 'legend'),
-    data=getMapData(map)) {
+addControl = function(
+  map, html, position = c('topleft', 'topright', 'bottomleft', 'bottomright'),
+  controlId = NULL, classes = c('info', 'legend'), data = getMapData(map)
+) {
 
-    position <- match.arg(position)
+    position = match.arg(position)
 
-    classes <- paste(classes, collapse=' ')
-    deps <- htmltools::resolveDependencies(htmltools::findDependencies(html))
-    html <- as.character(html)
+    classes = paste(classes, collapse = ' ')
+    deps = htmltools::resolveDependencies(htmltools::findDependencies(html))
+    html = as.character(html)
 
-    map$dependencies <- c(map$dependencies, deps)
+    map$dependencies = c(map$dependencies, deps)
     invokeMethod(map, data, 'addControl', html, position, controlId, classes)
 }
 
