af614887a4e82c077bf4b2d619f3d8ed84a83b44
Yihui Xie <xie@yihui.name>
2015-05-21 00:01:26 -0500

    move the environment variable from .Renviron to compile.R

--- compile.R
+++ compile.R
@@ -1,5 +1,6 @@
 set.seed(0102)
 options(htmlwidgets.TOJSON_ARGS = list(pretty = TRUE))
+Sys.setenv(R_KNITR_OPTIONS = 'knitr.chunk.tidy = FALSE')
 knitr::opts_chunk$set(out.width = '100%')
 library(leaflet)
 f = rmarkdown::render(commandArgs(TRUE))
