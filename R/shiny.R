ae84dce1706d16a65547790af89166f8ae97b6a3
Joe Cheng <joe@rstudio.com>
2015-06-19 10:41:13 -0700

    wip

--- R/shiny.R
+++ R/shiny.R
@@ -29,3 +29,34 @@ renderLeaflet = function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) expr = substitute(expr)  # force quoted
   htmlwidgets::shinyRenderWidget(expr, leafletOutput, env, quoted = TRUE)
 }
+
+#' @export
+leafletFullPage <- function(..., title = NULL, theme = NULL,
+  header = NULL, headerHeight = 60, footer = NULL, footerHeight = 45) {
+
+  absStyle <- function(top = "0", right = "0", bottom = "0", left = "0") {
+    sprintf("position: absolute; top: %s; right: %s; bottom: %s; left: %s;",
+      top, right, bottom, left)
+  }
+
+  headerHeight <- if (!is.null(header)) validateCssUnit(headerHeight) else "0"
+  footerHeight <- if (!is.null(footer)) validateCssUnit(footerHeight) else "0"
+
+  fluidPage(
+    title = title,
+    tags$style("html, body { width: 100%; height: 100%; }"),
+    if (!is.null(header)) {
+      div(style = absStyle(bottom = headerHeight),
+        header
+      )
+    },
+    div(style = absStyle(top = headerHeight, bottom = footerHeight),
+      ...
+    ),
+    if (!is.null(footer)) {
+      div(style = absStyle(top = footerHeight),
+        footer
+      )
+    }
+  )
+}
