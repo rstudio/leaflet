b955468c9bf0565832206e9828e0fe6012deba51
Bhaskar Karambelkar <bkarambelkar@threatconnect.com>
2015-09-04 12:56:52 -0400

    Support for Leaflet.Terminator plugin

---
+++ R/plugin-terminator.R
@@ -0,0 +1,45 @@
+leafletTerminatorDependencies <- function() {
+  list(
+    htmltools::htmlDependency(
+      "leaflet-terminator",
+      "0.1.0",
+      system.file("htmlwidgets/plugins/Leaflet.Terminator", package = "leaflet"),
+      script = "L.Terminator.js"
+    )
+  )
+}
+
+#' Add a daylight layer on top of the map
+#'
+#' @param map a map widget object
+#' @param
+#' resolution,time
+#' see \url{https://github.com/joergdietrich/Leaflet.Terminator}
+#' @param layerId the layer id
+#' @param group the name of the group this layer belongs to.
+#' @examples
+#' library(leaflet)
+#'
+#' leaf <- leaflet() %>%
+#'   addTiles() %>%
+#'   addTerminator()
+#'
+#' @export
+addTerminator <- function(
+  map,
+  resolution = 2,
+  time = NULL,
+  layerId = NULL,
+  group=NULL
+) {
+  map$dependencies <- c(map$dependencies, leafletTerminatorDependencies())
+  invokeMethod(
+    map
+    , getMapData(map)
+    , 'addTerminator'
+    , resolution
+    , time
+    , layerId
+    , group
+  )
+}
