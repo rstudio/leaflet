19c917d749a118e80393e2ae8515f65c3b6c8082
gherardovarando <gherardo.varando@gmail.com>
2015-09-07 09:32:19 +0200

    -add/remove DrawToolbar -options:   - is possible to choose to include or not any of the toolbar elements   - position   - editable drawing or not - object are passed to shiny

--- R/plugin-draw.R
+++ R/plugin-draw.R
@@ -8,7 +8,7 @@ leafletDrawDependencies <- function() {
       system.file("htmlwidgets/lib/Leaflet.draw/dist/", package = "leaflet"),
       script = "leaflet.draw.js",
       stylesheet="leaflet.draw.css"
-      ),
+    ),
     htmltools::htmlDependency(
       "leaflet-draw-plugin",
       "0.2.3",
@@ -18,19 +18,35 @@ leafletDrawDependencies <- function() {
 }
 
 
-#' Add drawing toolbar
+#' Add/remove drawing toolbar
 #'
-#' @param map the map to add the toolbar to
+#' Leaflet.draw plugin:
+#' https://github.com/Leaflet/Leaflet.draw
+#' @param map the map to add/remove the toolbar to/from
 #' @return modified map object
 #' @export
 #' @examples
 #' map <- addTiles(leaflet())
 #' map <- addDrawToolbar(map)
 #' map
-addDrawToolbar = function(map) {
- map$dependencies <- c(map$dependencies, leafletDrawDependencies())
- invokeMethod(map,getMapData(map),method =  'addDrawToolbar')
+addDrawToolbar <- function(map,layerID="drawnItems",
+                           position = c('topleft', 'topright', 'bottomleft',
+                                        'bottomright'),
+                           polyline=TRUE,polygon=TRUE,rectangle=TRUE,
+                           circle=TRUE,marker=TRUE,edit=TRUE) {
+
+  position = match.arg(position)
+  map$dependencies <- c(map$dependencies, leafletDrawDependencies())
+  map$drawToolbar<-T
+  invokeMethod(map,getMapData(map),method =  'addDrawToolbar',layerID,position,
+               polyline,polygon,rectangle,circle,marker,edit)
 }
 
+#' @describeIn addDrawToolbar
+#' @export
+removeDrawToolbar <- function(map){
+  map$dependencies <- c(map$dependencies, leafletDrawDependencies())
+  invokeMethod(map,getMapData(map),method =  'removeDrawToolbar')
+}
 
 
--- inst/htmlwidgets/plugins/leaflet-draw-plugin/leaflet-draw-plugin.js
+++ inst/htmlwidgets/plugins/leaflet-draw-plugin/leaflet-draw-plugin.js
@@ -1,18 +1,51 @@
-LeafletWidget.methods.addDrawToolbar = function(){
+LeafletWidget.methods.addDrawToolbar = function(layerID,position,polyline,polygon,rectangle,circle,marker,edit){
+  if (this.drawControl) {
+    this.drawControl.removeFrom(this);
+  }
   var drawnItems = new L.FeatureGroup();
-  this.layerManager.addLayer(drawnItems,"drawnItems");
+  this.layerManager.addLayer(drawnItems,"drawnItems",layerID);
+  if (edit) {
+    edit = { featureGroup: drawnItems};
+  }
   var drawControl = new L.Control.Draw({
-    edit: {
-      featureGroup: drawnItems
+    edit: edit,
+    position: position,
+    draw: {
+      polyline: polyline,
+      polygon: polygon,
+      rectangle: rectangle,
+      circle: circle,
+      marker: marker
     }
   });
-  this.addControl(drawControl);
+  this.drawControl = drawControl;
+  this.drawControl.addTo(this);
+  this.elemIx = 0;
   this.on('draw:created', function (e) {
     var type = e.layerType,
-				layer = e.layer;
-			  drawnItems.addLayer(layer);
+		layer = e.layer;
+    if (type== "polyline"){
+      var inflayer = layer.getLatLngs();
+    }
+    if (type == "marker"){
+      var inflayer = layer.getLatLng();
+    }
+    if (type == "rectangle"){
+      var inflayer = layer.getLatLngs();
+    }
+    if (type == "polygon"){
+      var inflayer = layer.getLatLngs();
+    }
+    if (type == "circle"){
+      var inflayer = [layer.getLatLng(), layer.getRadius()];
+    }
+		drawnItems.addLayer(layer);
+    if (!HTMLWidgets.shinyMode) return;
+    Shiny.onInputChange(layerID + type, inflayer);
+
   });
 };
 
-
-
+LeafletWidget.methods.removeDrawToolbar = function(){
+  this.drawControl.removeFrom(this);
+}
