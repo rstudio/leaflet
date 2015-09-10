1219d1508a0faad5b03abba9c5036f60120d4481
gherardovarando <gherardo.varando@gmail.com>
2015-09-10 08:38:15 +0200

    leaflet draw version corrected

--- R/plugin-draw.R
+++ R/plugin-draw.R
@@ -4,14 +4,14 @@ leafletDrawDependencies <- function() {
   list(
     htmltools::htmlDependency(
       "Leaflet.draw",
-      "1.0.0",
+      "0.2.3",
       system.file("htmlwidgets/lib/Leaflet.draw/dist/", package = "leaflet"),
       script = "leaflet.draw.js",
       stylesheet="leaflet.draw.css"
     ),
     htmltools::htmlDependency(
       "leaflet-draw-plugin",
-      "0.2.3",
+      "0.0.1",
       system.file("htmlwidgets/plugins/leaflet-draw-plugin/", package = "leaflet"),
       script = "leaflet-draw-plugin.js"
     )    )
