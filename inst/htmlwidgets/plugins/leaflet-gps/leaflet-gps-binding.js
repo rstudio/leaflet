2e76754fb022f4ff697a2f1b5c86f88b7f774f5f
zhe00a-vc <Bangyou.Zheng@csiro.au>
2016-04-26 09:39:16 +1000

    Move GPS plugin into another package leafletplugins to simplify the leaflet package.
    Update the leaflet.draw library to support the touch screen.

--- inst/htmlwidgets/plugins/leaflet-gps/leaflet-gps-binding.js
+++
@@ -1,9 +0,0 @@
-  LeafletWidget.methods.addControlGPS = function() {
-    (function() {
-      if(this.gpscontrol) {
-        this.gpscontrol.removeFrom( this );
-      }
-        this.gpscontrol = new L.Control.Gps();
-        this.gpscontrol.addTo(this);
-    }).call(this);
-  };
