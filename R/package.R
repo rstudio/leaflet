79469e014115bdb5b356f342e60d917d83f59cdc
Bhaskar V. Karambelkar <bhaskarvk@users.noreply.github.com>
2016-09-06 02:50:43 -0400

    Fixes #7 (#285)

--- R/package.R
+++ R/package.R
@@ -1,5 +1,9 @@
 #' @importFrom magrittr %>%
 #' @export %>%
 #' @importFrom htmlwidgets JS
+#' @importFrom grDevices col2rgb rgb
+#' @importFrom methods substituteDirect
+#' @importFrom stats na.omit quantile
+#' @importFrom utils getFromNamespace packageVersion
 #' @export JS
 NULL
