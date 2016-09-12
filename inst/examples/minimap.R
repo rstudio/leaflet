8c270bb9f8742a94c8ec6c53bcf5e78cb2eda29c
Bhaskar Karambelkar <bhaskar@karambelkar.info>
2016-09-12 13:26:20 -0400

    Upgraded minimap to 3.3.1 and added ability to add custom basemap for the minimap.

--- inst/examples/minimap.R
+++ inst/examples/minimap.R
@@ -1,6 +1,13 @@
 library(leaflet)
-# Default
-leaflet() %>% addTiles() %>% addMiniMap()
 
-# Custom Options
-leaflet() %>% addTiles() %>% addMiniMap(toggleDisplay = T)
+l <- leaflet() %>% setView(0,0,1)
+
+#' Default Minimap
+l %>% addTiles() %>% addMiniMap()
+
+#' <br/>
+#' Different basemap for the minimap and togglable
+l %>% addProviderTiles(providers$Esri.WorldStreetMap) %>%
+  addMiniMap(
+             tiles = providers$Esri.WorldStreetMap,
+             toggleDisplay = T)
