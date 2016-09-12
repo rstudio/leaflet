0303bd0cc9e64480270c78a5493148c33b8f629b
Bhaskar Karambelkar <bhaskar@karambelkar.info>
2016-09-12 17:05:35 -0400

    Documentation for knitting.

--- inst/examples/graticule.R
+++ inst/examples/graticule.R
@@ -1,13 +1,16 @@
 library(leaflet)
 # Default
-l <- leaflet() %>% addTiles() %>% setView(0,0,1)
+l <- leaflet() %>% addTiles() %>% setView(0,0,2)
 
+#' Default Graticule
 l %>% addGraticule()
 
-# Custom Params
+#' <br/>
+#' Custom Params
 l %>% addGraticule(interval = 40, style = list(color='#FF0000', weight=1))
 
-# on a toggleable Layer
+#' <br/>
+#' on a toggleable Layer
 l %>%
   addGraticule(group="graticule") %>%
   addLayersControl(
