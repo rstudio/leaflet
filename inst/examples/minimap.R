f392477238406a837f4a3b2c540a283a114de309
Bhaskar Karambelkar <bhaskar@karambelkar.info>
2016-09-14 09:26:58 -0400

    More complex examples and Fixed #11

--- inst/examples/minimap.R
+++ inst/examples/minimap.R
@@ -32,5 +32,104 @@ m %>%
   addMiniMap(
              tiles = esri[[1]],
              toggleDisplay = T) %>%
-  htmlwidgets::onRender("function(el, x) { var myMap = this; this.on('baselayerchange', function (e) { myMap.minimap.changeLayer(L.tileLayer.provider(e.name)); }) }")
+  htmlwidgets::onRender("function(el, x) {
+                            var myMap = this;
+                            this.on('baselayerchange',
+                                    function (e) {
+                                        myMap.minimap.changeLayer(L.tileLayer.provider(e.name));
+                                    })
+                        }")
 
+#' <br/><br/>
+#' Another advanced use case
+#' Minimap with markers.
+#' Note the use of 'group' ID to find markers in main map and add corresponding CircleMarkers in minimap.
+#' The V8 part is simply to read the JSON embeded in the Javascript.<br/>
+#' For a geojson file `jsonlite::fromfromJSON()` or `geojsonio::regeojson_read()` will do
+#'
+jsURL <- 'https://rawgit.com/Norkart/Leaflet-MiniMap/master/example/local_pubs_restaurant_norway.js'
+v8 <- V8::v8()
+v8$source(jsURL)
+geoJson <- geojsonio::as.json(v8$get('pubsGeoJSON'))
+
+# This is the kicker, convert geojson to a Spatial object.
+# This then allows us to use formulas in our markers, polygons etc.
+spdf <- geojsonio::geojson_sp(geoJson)
+
+icons <- awesomeIconList(
+  pub = makeAwesomeIcon(icon='glass', library='fa', markerColor = 'red'),
+  restaurant = makeAwesomeIcon(icon='cutlery', library='fa', markerColor = 'blue')
+)
+
+leaflet() %>% addTiles() %>%
+  setView(10.758276373601069, 59.92448055859924, 13) %>%
+  addAwesomeMarkers(data=spdf,
+             label=~stringr::str_c(amenity,': ', name),
+             icon = ~icons[amenity],
+             options = markerOptions(riseOnHover = TRUE, opacity = 0.75),
+             group = 'pubs') %>%
+  addMiniMap() %>%
+  htmlwidgets::onRender("function(el, t) {
+                             var myMap = this;
+
+                             var pubs = myMap.layerManager._byGroup.pubs;
+                             var pubs2 = new L.FeatureGroup();
+
+                             for(pub in pubs) {
+                                 var m = new L.CircleMarker(pubs[pub]._latlng,
+                                                            {radius: 2});
+                                 pubs2.addLayer(m);
+
+                             }
+                             var layers = new L.LayerGroup([myMap.minimap._layer, pubs2]);
+
+                             myMap.minimap.changeLayer(layers);
+                        }")
+
+#' <br/><br/>
+#' Finally combine the approaches in last 2 examples
+#' Minimap w/ changable layers and circle markers.
+m <- leaflet()
+esri <- providers %>%
+  purrr::keep(~ grepl('^Esri',.))
+
+esri %>%
+  purrr::walk(function(x) m <<- m %>% addProviderTiles(x,group=x))
+
+m %>%
+  setView(10.758276373601069, 59.92448055859924, 13) %>%
+  addAwesomeMarkers(data=spdf,
+             label=~stringr::str_c(amenity,': ', name),
+             icon = ~icons[amenity],
+             options = markerOptions(riseOnHover = TRUE, opacity = 0.75),
+             group = 'pubs') %>%
+  addLayersControl(
+    baseGroups = names(esri),
+    options = layersControlOptions(collapsed = FALSE)
+  ) %>%
+  addMiniMap(tiles = esri[[1]],
+             toggleDisplay = T) %>%
+  htmlwidgets::onRender("function(el, t) {
+                             var myMap = this;
+
+                             var pubs = myMap.layerManager._byGroup.pubs;
+                             var pubs2 = new L.FeatureGroup();
+
+                             for(pub in pubs) {
+                                 var m = new L.CircleMarker(pubs[pub]._latlng,
+                                                            {radius: 2});
+                                 pubs2.addLayer(m);
+
+                             }
+                             var layers = new L.LayerGroup([myMap.minimap._layer, pubs2]);
+
+                             myMap.minimap.changeLayer(layers);
+
+                             myMap.on('baselayerchange',
+                                      function (e) {
+                                          debugger;
+                                          myMap.minimap.changeLayer(
+                                             new L.LayerGroup([L.tileLayer.provider(e.name),
+                                                pubs2]));
+                                      });
+                        }")
