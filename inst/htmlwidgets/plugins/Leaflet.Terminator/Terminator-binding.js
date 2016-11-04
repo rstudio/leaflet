a40e623c133e3353e7ea0d14207b0f95ab4df613
Bhaskar Karambelkar <bhaskar@karambelkar.info>
2016-11-04 10:42:49 -0400

    Made Graticule and Terminator unclickable by default. This is necessary for highlighting shapes properly.

--- inst/htmlwidgets/plugins/Leaflet.Terminator/Terminator-binding.js
+++ inst/htmlwidgets/plugins/Leaflet.Terminator/Terminator-binding.js
@@ -1,11 +1,12 @@
-  LeafletWidget.methods.addTerminator = function(resolution, time, layerId, group) {
+  LeafletWidget.methods.addTerminator = function(resolution, time,
+  layerId, group, options) {
     (function() {
       this.layerManager.addLayer(
-        L.terminator({
+        L.terminator($.extend({
           resolution: resolution,
           time: time,
           group: group
-        }),
+        }), options || {}),
         'shape', layerId, group);
     }).call(this);
   };
