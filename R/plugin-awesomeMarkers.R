f0c0b286ddf17d9fa3c8ff65f7d8cd4b6f642c4b
Bhaskar V. Karambelkar <bhaskarvk@users.noreply.github.com>
2016-12-14 17:03:05 -0500

    Upgraded to Latest Awesome Markers (#340)

--- R/plugin-awesomeMarkers.R
+++ R/plugin-awesomeMarkers.R
@@ -2,7 +2,7 @@ leafletAwesomeMarkersDependencies <- function() {
   list(
     htmltools::htmlDependency(
       "leaflet-awesomemarkers",
-      "2.0.2",
+      "2.0.3",
       system.file("htmlwidgets/plugins/Leaflet.awesome-markers", package = "leaflet"),
       script = c('leaflet.awesome-markers.min.js'),
       stylesheet = c('leaflet.awesome-markers.css')
@@ -13,8 +13,8 @@ leafletAwesomeMarkersDependencies <- function() {
 leafletAmBootstrapDependencies <- function(map) {
   list(
     htmltools::htmlDependency(
-      "awesomemarkers-bootstrap",
-      "3.3.5",
+      "bootstrap",
+      "3.3.7",
       system.file("htmlwidgets/plugins/Leaflet.awesome-markers", package = "leaflet"),
       script = c('bootstrap.min.js'),
       stylesheet = c('bootstrap.min.css')
@@ -32,8 +32,8 @@ addBootstrap <- function(map) {
 leafletAmFontAwesomeDependencies <- function(map) {
   list(
     htmltools::htmlDependency(
-      "awesomemarkers-fontawesome",
-      "4.4.0",
+      "fontawesome",
+      "4.7.0",
       system.file("htmlwidgets/plugins/Leaflet.awesome-markers", package = "leaflet"),
       stylesheet = c('font-awesome.min.css')
     )
@@ -50,8 +50,8 @@ addFontAwesome <- function(map) {
 leafletAmIonIconDependencies <- function(map) {
   list(
     htmltools::htmlDependency(
-      "awesomemarkers-ionicons",
-      "2.0.0",
+      "ionicons",
+      "2.0.1",
       system.file("htmlwidgets/plugins/Leaflet.awesome-markers", package = "leaflet"),
       stylesheet = c('ionicons.min.css')
     )
@@ -158,9 +158,10 @@ makeAwesomeIcon <- function(
 #' @param icon Name of the icon
 #' @param library Which icon library. Default \code{'glyphicon'}, other possible
 #'   values are \code{'fa'} (fontawesome) or \code{'ion'} (ionicons).
-#' @param markerColor Possible values are \code{'red'}, \code{'darkred'},
-#'   \code{'orange'}, \code{'green'}, \code{'darkgreen'}, \code{'blue'},
-#'   \code{'purple'}, \code{'darkpuple'}, \code{'cadetblue'}
+#' @param markerColor Possible values are \code{'red'}, \code{'darkred'}, \code{'lightred'}, \code{'orange'},
+#' \code{'beige'}, \code{'green'}, \code{'darkgreen'}, \code{'lightgreen'}, \code{'blue'}, 
+#' \code{'darkblue'}, \code{'lightblue'}, \code{'purple'}, \code{'darkpurple'}, \code{'pink'}, 
+#' \code{'cadetblue'}, \code{'white'}, \code{'gray'}, \code{'lightgray'}, \code{'black'}
 #' @param iconColor The color to use for the icon itself. Use any CSS-valid
 #'   color (hex, rgba, etc.) or a named web color.
 #' @param spin If \code{TRUE}, make the icon spin (only works when \code{library
