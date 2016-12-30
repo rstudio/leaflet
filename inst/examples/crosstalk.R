a6483577982e13f627d672757d89afe953a238e9
timelyportfolio <kent.russell@timelyportfolio.com>
2016-12-30 12:01:24 -0600

    add simple crosstalk example

---
+++ inst/examples/crosstalk.R
@@ -0,0 +1,34 @@
+# devtools::install_github("rstudio/crosstalk@joe/simplify")
+# devtools::install_github("rstudio/leaflet@crosstalk3")
+
+library(crosstalk)
+library(leaflet)
+library(htmlwidgets)
+
+rand_lng = function(n = 10) rnorm(n, -93.65, .01)
+rand_lat = function(n = 10) rnorm(n, 42.0285, .01)
+
+pts <- SharedData$new(
+  data.frame(
+    lng = rand_lng(),
+    lat = rand_lat()
+  ),
+  group = "grp1"
+)
+
+
+lf <- leaflet(pts) %>%
+  addTiles() %>%
+  addMarkers()
+
+
+onRender(
+  lf,
+"
+function(el,x) {
+  debugger;
+  var sl = new crosstalk.SelectionHandle('grp1');
+  sl.on('change', function(val){console.log(val);})
+}
+"
+)
