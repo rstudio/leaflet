9261eca5dcbd657c7f8eaa0bbf97b29431819f79
timelyportfolio <kent.russell@timelyportfolio.com>
2016-12-30 13:24:35 -0600

    hack to make leaflet only render once

--- inst/examples/crosstalk_shiny.R
+++ inst/examples/crosstalk_shiny.R
@@ -1,5 +1,5 @@
-# devtools::install_github("rstudio/crosstalk@joe/simplify")
-# devtools::install_github("rstudio/leaflet@crosstalk3")
+# devtools::install_github("rstudio/crosstalk")
+# devtools::install_github("rstudio/leaflet")
 
 library(crosstalk)
 library(leaflet)
@@ -11,7 +11,7 @@ rand_lat = function(n = 10) rnorm(n, 42.0285, .01)
 
 ui <- fluidPage(
   fluidRow(
-    column(2,   filter_select(id="filterselect", label="Points", sharedData=pts, group=~id)),
+    column(2, filter_select(id="filterselect", label="Points", sharedData=pts, group=~id)),
     column(6, leafletOutput("leaflet1"))
   ),
   h4("Selected points"),
@@ -29,15 +29,23 @@ server <- function(input, output, session) {
     group = "grp1"
   )
 
+
+
+  lf <- leaflet(pts) %>%
+    addTiles() %>%
+    addMarkers()
+
+  not_rendered <- TRUE
+
   output$leaflet1 <- renderLeaflet({
-    leaflet(pts) %>%
-      addTiles() %>%
-      addMarkers()
+    if(req(not_rendered,cancelOutput=TRUE)) {
+      not_rendered <- FALSE
+      lf
+    }
   })
 
   output$selectedpoints <- renderPrint({
     df <- pts$data(withSelection = TRUE)
-
     cat(nrow(df), "observation(s) selected\n\n")
     str(dplyr::glimpse(df))
   })
