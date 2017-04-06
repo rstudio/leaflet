00bb41c179356f0452b09b02fd03bf854c00d322
Joe Cheng <joe@rstudio.com>
2017-04-06 10:29:22 -0700

    metaData.SharedData wasn't calling metaData itself

--- R/normalize-SharedData.R
+++ R/normalize-SharedData.R
@@ -1,6 +1,6 @@
 #' @export
 metaData.SharedData <- function(obj) {
-  obj$data(withSelection = TRUE, withFilter = FALSE, withKey = TRUE)
+  metaData(obj$data(withSelection = TRUE, withFilter = FALSE, withKey = TRUE))
 }
 
 #' @export
