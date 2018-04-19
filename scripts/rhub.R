7c5a53cc098932237d267ba68522c2c1765f9159
Barret Schloerke <schloerke@gmail.com>
2018-04-19 13:49:21 -0400

    check all cran platforms and windows release. run donttest as well.

--- scripts/rhub.R
+++ scripts/rhub.R
@@ -3,4 +3,11 @@ source("scripts/git_clean.R")
 if (!require("rhub", quietly = TRUE)) install.packages("rhub")
 
 build_file <- rhub:::build_package(".", "../builds")
-rhub::check_for_cran(build_file, email = "barret@rstudio.com")
+
+rhub::check_for_cran(
+  build_file,
+  email = "barret@rstudio.com",
+  platforms = c("windows-x86_64-release", rhub:::default_cran_check_platforms(build_file)),
+  env_vars = c("_R_CHECK_FORCE_SUGGESTS_" = "0"),
+  check_args = "--run-donttest"
+)
