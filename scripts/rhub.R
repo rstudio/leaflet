ea6fe7d931d90883a60cb34c28a441269d443d62
Barret Schloerke <schloerke@gmail.com>
2018-04-19 11:32:57 -0400

    update rhub script

--- scripts/rhub.R
+++ scripts/rhub.R
@@ -1,6 +1,6 @@
 source("scripts/git_clean.R")
 
-if (!require("rhub")) install.packages("rhub")
+if (!require("rhub", quietly = TRUE)) install.packages("rhub")
 
 build_file <- rhub:::build_package(".", "../builds")
-rhub::check_for_cran(build_file)
+rhub::check_for_cran(build_file, email = "barret@rstudio.com")
