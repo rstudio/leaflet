aaae3ca2d6349d8008dfe486b2332689c58b81c0
Barret Schloerke <schloerke@gmail.com>
2018-04-19 16:30:26 -0400

    show status for all logs and display final output

--- scripts/rhub.R
+++ scripts/rhub.R
@@ -4,10 +4,19 @@ if (!require("rhub", quietly = TRUE)) install.packages("rhub")
 
 build_file <- rhub:::build_package(".", "../builds")
 
-rhub::check_for_cran(
+platforms <- c("windows-x86_64-release", rhub:::default_cran_check_platforms(build_file))
+check_output <- rhub::check_for_cran(
   build_file,
   email = "barret@rstudio.com",
-  platforms = c("windows-x86_64-release", rhub:::default_cran_check_platforms(build_file)),
+  platforms = platforms,
   env_vars = c("_R_CHECK_FORCE_SUGGESTS_" = "0"),
-  check_args = "--run-donttest"
+  show_status = FALSE
 )
+
+for (i in seq_along(platforms)) {
+  check_output$livelog(i)
+}
+
+# check_output$web()
+
+print(check_output)
