8d76506f88b15da45def9c35caf9efd75f64ae31
Barret Schloerke <schloerke@gmail.com>
2018-06-04 10:25:35 -0400

    compare against cran

--- scripts/viztest.R
+++ scripts/viztest.R
@@ -3,6 +3,7 @@ devtools::install_github("schloerke/viztest")
 
 
 # compare to leaflet.js v0.7.x
-viztest::viztest(".", "rstudio/leaflet@d489e2c", resize = FALSE, stomp = TRUE)
+# viztest::viztest(".", "rstudio/leaflet@d489e2c", resize = FALSE, stomp = TRUE)
+viztest::viztest(".", "leaflet", resize = FALSE, stomp = TRUE)
 
 # viztest::viztest(".", "rstudio/leaflet", resize = FALSE, stomp = TRUE, skip_old = TRUE)
