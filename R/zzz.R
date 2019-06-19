03742af1af820993a8484208b0a53a4cc3446469
leslie-huang <lesliehuang@nyu.edu>
2019-06-19 15:40:17 -0400

    include version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   providers <<- leaflet.providers::providers()$providers
   providers.details <<- leaflet.providers::providers()$providers_details
+  providers.version_num <<- leaflet.providers::providers()$version_num
 }
