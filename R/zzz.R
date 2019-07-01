b379656f5aaa3901caaa9f4b9ef4db8e9af863dc
leslie-huang <lesliehuang@nyu.edu>
2019-07-01 11:48:31 -0400

    remove onLoad

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-  providers_info <- leaflet.providers::providers()
-
-  providers <<- providers_info$providers
-  providers.details <<- providers_info$providers_details
-  providers.version_num <<- providers_info$version_num
-  providers.src <<- providers_info$src
-}
