67085a0c0a5ce408bcd762a26c8285de5ba998ad
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-14 10:52:15 -0400

    refactor(assure_crs_terra): Return early faster if not enough info

--- R/normalize-terra.R
+++ R/normalize-terra.R
@@ -54,17 +54,20 @@ assure_crs_terra <- function(x) {
   stopifnot(is_installed("terra"))
 
   prj <- raster::crs(x, proj = TRUE)
+
+  if (is.na(prj) || (prj == "")) {
+    # Don't have enough information to check
+    warning("SpatVector layer is not long-lat data", call. = FALSE)
+    return(x)
+  }
+
   if (terra::is.lonlat(x, perhaps = TRUE, warn = FALSE)) {
     if (!grepl("+datum=WGS84", prj, fixed = TRUE)) {
       x <- terra::project(x, "+proj=longlat +datum=WGS84")
     }
     return(x)
   }
-  # Don't have enough information to check
-  if (is.na(prj) || (prj == "")) {
-    warning("SpatVector layer is not long-lat data", call. = FALSE)
-    return(x)
-  }
+
   terra::project(x, "+proj=longlat +datum=WGS84")
 }
 
