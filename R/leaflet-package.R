d1354fd33ab3308a4486302f237d8de87602094a
Garrick Aden-Buie <garrick@adenbuie.com>
2023-08-14 10:23:18 -0400

    chore: use_package_doc() and relocate imports

---
+++ R/leaflet-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom grDevices col2rgb
+#' @importFrom grDevices rgb
+#' @importFrom htmlwidgets JS
+#' @importFrom magrittr %>%
+#' @importFrom methods substituteDirect
+#' @importFrom stats na.omit
+#' @importFrom stats quantile
+#' @importFrom utils getFromNamespace
+#' @importFrom utils packageVersion
+## usethis namespace: end
+NULL
+
+## Re-exports
+#' @export %>%
+#' @export JS
+NULL
