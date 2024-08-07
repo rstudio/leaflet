b882630bdabf85b8ddfe89eeadaad52d22872c8c
Barret Schloerke <barret@posit.co>
2024-08-07 13:16:13 -0400

    Fix arg name

--- tests/testthat/test-tiles.R
+++ tests/testthat/test-tiles.R
@@ -5,7 +5,7 @@ testthat::test_that("Checking of tile providers works correctly", {
   )
 
   expect_no_error(
-    leaflet() %>% addProviderTiles("FAKETILESET123", .check = FALSE)
+    leaflet() %>% addProviderTiles("FAKETILESET123", check = FALSE)
   )
 
   expect_error(
