094e5aa9a177beaa37895361e534fd092a4e9078
Matt Neilson <mneilson2@gmail.com>
2024-08-26 23:17:27 -0400

    Update Leaflet.markercluster to 1.4.1

--- R/layers.R
+++ R/layers.R
@@ -1000,7 +1000,7 @@ markerClusterDependencies <- function() {
   list(
     htmltools::htmlDependency(
       "leaflet-markercluster",
-      "1.0.5",
+      "1.4.1",
       "htmlwidgets/plugins/Leaflet.markercluster",
       package = "leaflet",
       script = c(
